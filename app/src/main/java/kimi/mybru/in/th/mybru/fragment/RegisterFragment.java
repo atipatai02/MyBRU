c485b9a022ffc0477c4d830436f7614ddfa904bf
test <atipatai02@gmail.com>
2018-04-26 16:32:10 +0700

    List Food

--- app/src/main/java/kimi/mybru/in/th/mybru/fragment/RegisterFragment.java
+++ app/src/main/java/kimi/mybru/in/th/mybru/fragment/RegisterFragment.java
@@ -5,14 +5,23 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import kimi.mybru.in.th.mybru.MainActivity;
 import kimi.mybru.in.th.mybru.R;
+import kimi.mybru.in.th.mybru.utility.AddNewUserToServer;
+import kimi.mybru.in.th.mybru.utility.MyAlert;
+import kimi.mybru.in.th.mybru.utility.MyConstant;
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
 
 
     @Override
@@ -25,9 +34,73 @@ public class RegisterFragment extends Fragment{
 
     }   // Main Method
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            uploadValueToServer();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToServer() {
+
+//        get value from EditText
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//        Change Data Type From Edittext to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+//        Check Space
+        if (nameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) {
+//            Have Space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space", "Please Fill All Blank");
+
+        } else {
+//            No Space
+            try {
+
+                MyConstant myConstant = new MyConstant();
+                AddNewUserToServer addNewUserToServer = new AddNewUserToServer(getActivity());
+                addNewUserToServer.execute(nameString, userString, passwordString, myConstant.getUrlAddUser());
+
+                String result = addNewUserToServer.get();
+                Log.d("26AprilV1", "result ==>" + result);
+                if (Boolean.parseBoolean(result)) {
+                    getActivity().getSupportFragmentManager().popBackStack();
+                } else {
+                    Toast.makeText(getActivity(), "Error Cannot Upload",Toast.LENGTH_LONG).show();
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
     private void createToolbar() {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
 //        Setup Title
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -44,6 +117,8 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
 
     }
 
