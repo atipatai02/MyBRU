fd3a2fbbb8891535e6ee962af25480a3837d9873
test <atipatai02@gmail.com>
2018-04-26 17:41:40 +0700

    Show List Finish

--- app/src/main/java/kimi/mybru/in/th/mybru/fragment/ServiceFragment.java
+++ app/src/main/java/kimi/mybru/in/th/mybru/fragment/ServiceFragment.java
@@ -7,10 +7,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import kimi.mybru.in.th.mybru.R;
+import kimi.mybru.in.th.mybru.utility.FoodAdapter;
+import kimi.mybru.in.th.mybru.utility.GetAllData;
+import kimi.mybru.in.th.mybru.utility.MyConstant;
 
-public class ServiceFragment extends Fragment{
+public class ServiceFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create LListView
+        createLListView();
+
+    }   // Main Method
+
+    private void createLListView() {
+        ListView listView = getView().findViewById(R.id.listViewFood);
+        MyConstant myConstant = new MyConstant();
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myConstant.getUrlGetAllFood());
+
+            String jsonString = getAllData.get();
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            String[] foodStrings = new String[jsonArray.length()];
+            String[] priceStrings = new String[jsonArray.length()];
+            String[] detailStrings = new String[jsonArray.length()];
+            String[] imageStrings = new String[jsonArray.length()];
+
+            for (int i=0; i<jsonArray.length(); i+=1) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                foodStrings[i] = jsonObject.getString("NameFood");
+                priceStrings[i] = jsonObject.getString("Price");
+                detailStrings[i] = jsonObject.getString("Detail");
+                imageStrings[i] = jsonObject.getString("ImagePath");
+
+            }
+
+            FoodAdapter foodAdapter = new FoodAdapter(getActivity(),imageStrings,foodStrings,priceStrings,detailStrings);
+            listView.setAdapter(foodAdapter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     @Nullable
     @Override
